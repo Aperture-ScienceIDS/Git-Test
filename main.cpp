db651ab12083120bb242f633603a562aac4cbba5
Landon Thomas <thomasln4242@gmail.com>
2018-11-27 13:47:32 -0600

    Fourth commit: adding new main.cpp

--- main.cpp
+++ main.cpp
@@ -8,9 +8,23 @@
 
 using namespace std;
 
+int sum(int n){
+  int total = 0;
+  for(int i = 0; i <= n; i++)
+    {
+      total += i;
+    }
+  return total;
+}
 int main(){
 
-  cout << "Hello World!"<< endl;
+  cout << "Provide an integer: " << endl;
+  int n;
+  cin >> n;
+  //cout << "Hello World!"<< endl;
+
+  int total = sum(n);
+  cout << "The sum of 1 to " << n << " is: " << total << endl;
 
   return 0;
 
